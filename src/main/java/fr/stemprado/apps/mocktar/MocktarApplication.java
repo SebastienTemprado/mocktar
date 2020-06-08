181bf1492b04505c5fc93e355628069e9e10cf03
Sébastien Temprado <stemprado@gmail.com>
2020-06-08 22:08:24 +0200

    init

---
+++ src/main/java/fr/stemprado/apps/mocktar/MocktarApplication.java
@@ -0,0 +1,13 @@
+package fr.stemprado.apps.mocktar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocktarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MocktarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/stemprado/apps/mocktar/MocktarApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.stemprado.apps.mocktar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MocktarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
