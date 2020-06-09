3af1d7b54b85b889d56c45eb1a8b363474d03346
Sébastien Temprado <stemprado@gmail.com>
2020-06-09 23:29:17 +0200

    add vuejs + axios

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -5,3 +5,20 @@ function addMock() {
 function removeMock() {
     alert("mock removed!");
 }
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    },
+    created: function() {
+        var vm = this;
+        axios.get('https://yesno.wtf/api')
+        .then(function (response) {
+            vm.message = response.data.answer;
+        })
+        .catch(function (error) {
+            vm.message = 'Error! Could not reach the API. ' + error;
+        })
+    }
+})
