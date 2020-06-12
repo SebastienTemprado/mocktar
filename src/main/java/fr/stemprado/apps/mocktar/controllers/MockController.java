b9af5343714a9d7c6ee8c0bdc0522a3a4ce46ed0
Sébastien Temprado <stemprado@gmail.com>
2020-06-12 22:17:01 +0200

    add the endpoint to get any mock response

--- src/main/java/fr/stemprado/apps/mocktar/controllers/MockController.java
+++ src/main/java/fr/stemprado/apps/mocktar/controllers/MockController.java
@@ -2,16 +2,21 @@ package fr.stemprado.apps.mocktar.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.stemprado.apps.mocktar.beans.Mock;
+import fr.stemprado.apps.mocktar.exceptions.NotFoundException;
 import fr.stemprado.apps.mocktar.services.MockService;
 
 @RestController
@@ -34,5 +39,15 @@ public class MockController {
 	public void deleteMock(@PathVariable String name) {
 		mockService.deleteMock(name);
 	}
+
+	@RequestMapping("/mocktar/**")
+    public String handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String mockURI = request.getRequestURI().replace("/mocktar", "");
+		List<Mock> mocks = mockService.getMocks(null);
+		
+		Mock mock = mocks.stream().filter(m -> m.request.equals(mockURI)).findFirst().orElseThrow(() -> new NotFoundException());
+
+      	return mock.response;
+    }
 
 }
---
+++ src/main/java/fr/stemprado/apps/mocktar/exceptions/NotFoundException.java
@@ -0,0 +1,10 @@
+package fr.stemprado.apps.mocktar.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = -3169067747117693559L;
+}
